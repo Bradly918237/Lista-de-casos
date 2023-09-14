1a77ad54c1ce2191462a691a52da7b92880cff25
Bradly918237 <143900125+Bradly918237@users.noreply.github.com>
2023-09-14 11:46:04 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+/*2. Crea las variables nombre, dirección y teléfono y asígnale los valores correspondientes. Muestra
+los valores de esas variables por pantalla.*/
+public class Main {
+    public static void main(String[] args) {
+        String nombre = "Bradly";
+        String direccion = "Calle Bolognesi 1234";
+        String telefono = "123456789";
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Teléfono: " + telefono);
+    }
+}
