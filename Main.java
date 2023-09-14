69c94bdc0f81f0591c595c229b4497c0f6bf38c5
Bradly918237 <143900125+Bradly918237@users.noreply.github.com>
2023-09-14 11:53:45 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+/*3. Realiza un conversor de euros a pesetas. La cantidad en euros que se quiere convertir deberá
+estar almacenada en una variable.*/
+public class Main {
+    public static void main(String[] args) {
+        double euros = 100;
+        double pesetas = euros * 166.386;
+
+        System.out.println(euros + " euros son " + pesetas + " pesetas.");
+    }
+}
