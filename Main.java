37d0f34bc4cac6fdbd9a94b30512173de9920c85
Bradly918237 <143900125+Bradly918237@users.noreply.github.com>
2023-09-14 11:56:46 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+/*4. Realiza un conversor de soles a euros. La cantidad en soles que se quiere convertir deberá estar
+almacenada en una variable.*/
+public class Main {
+    public static void main(String[] args) {
+        double soles = 100;
+        double euros = soles / 4.08;
+
+        System.out.println(soles + " soles son " + euros + " euros.");
+    }
+}
