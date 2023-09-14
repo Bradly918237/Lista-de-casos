f2f59664a3a2fea805b27eb7d5ae2c0c56df243c
Bradly918237 <143900125+Bradly918237@users.noreply.github.com>
2023-09-14 12:12:47 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
-/*6. Realiza un programa que convierta valores de Mb a Kb.*/
+/*7. Realiza un programa que calcule la nota que hace falta sacar en el segundo examen de la
+asignatura Programación para obtener la media deseada. Hay que tener en cuenta que la nota
+del primer examen cuenta el 40% y la del segundo examen un 60%. En la imagen se muestra el
+funcionamiento del programa.*/
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int Mb = 10;
-        int Kb = Mb * 1024;
-        System.out.println(Mb + " Mb son " + Kb + " Kb.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la nota del primer examen: ");
+        double notaExamen1 = scanner.nextDouble();
+        System.out.print("Ingrese la nota del segundo examen: ");
+        double notaExamen2 = scanner.nextDouble();
+        System.out.print("Ingrese la nota deseada: ");
+        double notaDeseada = scanner.nextDouble();
+        double notaFaltante = notaDeseada - (notaExamen1 + notaExamen2);
+        System.out.println("La nota que falta para alcanzar la nota deseada es: " + notaFaltante);
+        scanner.close();
     }
 }
