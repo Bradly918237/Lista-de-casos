07627ead1106e0d1c2b997cc5a6e26b9c7ef8ab8
Bradly918237 <143900125+Bradly918237@users.noreply.github.com>
2023-09-14 12:02:14 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+/*6. Realiza un programa que convierta valores de Mb a Kb.*/
+public class Main {
+    public static void main(String[] args) {
+        int Mb = 10;
+        int Kb = Mb * 1024;
+        System.out.println(Mb + " Mb son " + Kb + " Kb.");
+    }
+}
