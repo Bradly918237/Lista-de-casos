799fd0b54f713d68135e1d137bd343370152f3cc
Bradly918237 <143900125+Bradly918237@users.noreply.github.com>
2023-09-15 05:13:30 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,35 @@
-/*7. Realiza un programa que calcule la nota que hace falta sacar en el segundo examen de la
-asignatura Programación para obtener la media deseada. Hay que tener en cuenta que la nota
-del primer examen cuenta el 40% y la del segundo examen un 60%. En la imagen se muestra el
-funcionamiento del programa.*/
+/*17. Realiza un programa que vaya pidiendo números hasta que se introduzca un numero negativo y
+nos diga cuantos números se han introducido, la media de los impares y el mayor de los pares.
+El número negativo sólo se utiliza para indicar el final de la introducción de datos, pero no se
+incluye en el cómputo*/
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese la nota deseada: ");
-        double notaDeseada = scanner.nextDouble();
-        System.out.print("Ingrese la nota del primer examen: ");
-        double notaPrimerExamen = scanner.nextDouble();
-        double notaSegundoExamen = (notaDeseada - (notaPrimerExamen * 0.4)) / 0.6;
-        System.out.println("La nota necesaria en el segundo examen es: " + notaSegundoExamen);
+        int contadorNumeros = 0;
+        int sumaImpares = 0;
+        int cantidadImpares = 0;
+        int mayorPar = Integer.MIN_VALUE;
+        while (true) {
+            System.out.print("Ingrese un número, ingresar negativo para salir: ");
+            int numero = scanner.nextInt();
+            if (numero < 0) {
+                break;
+            }
+            contadorNumeros++;
+            if (numero % 2 != 0) {
+                sumaImpares += numero;
+                cantidadImpares++;
+            }
+            if (numero % 2 == 0 && numero > mayorPar) {
+                mayorPar = numero;
+            }
+        }
+        double mediaImpares = (double) sumaImpares / cantidadImpares;
+        System.out.println("Cantidad de números introducidos: " + contadorNumeros);
+        System.out.println("Media de los números impares: " + mediaImpares);
+        System.out.println("Mayor de los números pares: " + mayorPar);
         scanner.close();
     }
 }
