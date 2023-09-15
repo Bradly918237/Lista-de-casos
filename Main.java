5cfa7f7359c93c95a7653a737242b14ad0f395d7
Bradly918237 <143900125+Bradly918237@users.noreply.github.com>
2023-09-15 05:21:41 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,24 @@
-/*7. Realiza un programa que calcule la nota que hace falta sacar en el segundo examen de la
-asignatura Programación para obtener la media deseada. Hay que tener en cuenta que la nota
-del primer examen cuenta el 40% y la del segundo examen un 60%. En la imagen se muestra el
-funcionamiento del programa.*/
+/*19. Realiza un programa que pinte la letra L por pantalla hecha con asteriscos. El programa pedirá la
+altura. El palo horizontal de la L tendrá una longitud de la mitad (división entera entre 2) de la
+altura más uno.*/
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese la nota deseada: ");
-        double notaDeseada = scanner.nextDouble();
-        System.out.print("Ingrese la nota del primer examen: ");
-        double notaPrimerExamen = scanner.nextDouble();
-        double notaSegundoExamen = (notaDeseada - (notaPrimerExamen * 0.4)) / 0.6;
-        System.out.println("La nota necesaria en el segundo examen es: " + notaSegundoExamen);
+        System.out.print("Ingrese la altura de la letra L: ");
+        int altura = scanner.nextInt();
+        int longitudPalo = altura / 2 + 1;
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < longitudPalo; j++) {
+                if (j == 0 || i == altura - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
         scanner.close();
     }
 }
